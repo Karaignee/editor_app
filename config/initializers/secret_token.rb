5c2a8aab74aa3088dcbd2256fa5bf2bfc2777004
Karaignee <karen_rooney@hotmail.com>
2014-05-07 17:24:36 +0100

    initial commit with POst scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Editor::Application.config.secret_key_base = 'eb5a974a96cdf7385fd199a603e21cb45265eeb87d3543c0aaa0cddb8ce81f75aa5431e5c01f1e1aecea2fac7f48dc526038da7cbfb8381a7dac8bf339290e8e'
